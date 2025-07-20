db0af6eba811a08839e036b88c78fa7411377eb8
Florian Mies <florian.mies@workist.com>
2025-07-20 13:25:56 +0200

    Make button do some js stuff: first hydration island. Why am I using Astro again?

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,30 @@
+"use client";
+
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Hero = () => {
+  const handleClick = () => {
+    alert("Button clicked!");
+    // Or do any other logic here
+  };
+
   return (
     <div className="font-gasoek bg-yellow-100 text-center py-20 h-full flex flex-col items-center justify-center cursor-cat actice:cursor-cell focus:cursor-cell">
       <h1 className=" font-bold text-yellow-800 text-9xl cursor-paw-hover">
         Gats Patraix
       </h1>
       <div className="flex justify-center mt-32 space-x-8">
-        <button className="text-6xl text-red-600 font-bold cursor-cat-hover">
+        <Button
+          className="text-6xl text-red-600 font-bold cursor-cat-hover"
+          onClick={handleClick}
+        >
           Adoption
-        </button>
-        <button className="text-6xl text-green-600 font-bold cursor-cat-walking-hover">
+        </Button>
+        <button
+          className="text-6xl text-green-600 font-bold cursor-cat-walking-hover"
+          onClick={handleClick}
+        >
           Fostering
         </button>
         <button className="text-6xl text-blue-600 font-bold cursor-cat-hover">
@@ -19,6 +33,7 @@ const Hero = () => {
         <button className="text-6xl text-purple-600 font-bold cursor-cat-walking-hover">
           Cats
         </button>
+        <Button onClick={handleClick}>Test</Button>
       </div>
     </div>
   );
